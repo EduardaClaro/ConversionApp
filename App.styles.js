bfa77fd91acf5a2870a4ef97b0c2fbe3f7cde9c8
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-08-05 13:48:40 -0300

    :sparkles: feat: add_app_styles

---
+++ App.styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+import { colors } from './src/styles/colors';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.background,
+    },
+    scrollView: {
+        flexGrow: 1,
+    },
+    content: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingVertical: 80,
+        paddingBottom: 24,
+    },
+    header: {
+        marginBottom: 32,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: colors.textSecondary,
+        marginBottom: 8,
+    },
+    subTitle: {
+        color: colors.textSecondary,
+        fontSize: 16,
+    },
+    card: {
+        backgroundColor: colors.cardBackground,
+        borderRadius: 16,
+        padding: 24,
+        marginBottom: 24,
+    },
+    label: {
+        color: colors.textSecondary,
+        fontSize: 14,
+        marginBottom: 8,
+    },
+    currencyGrid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginHorizontal: -4,
+        marginBottom: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
